5c7fa4a7b844544f6d04f8a951eecaec87483ab3
Gabrielle Dias <169068323+GABRIELLEDIAS19@users.noreply.github.com>
2024-05-13 20:38:10 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Curso Técnico: Desenvolvimento de Sistemas");
+    System.out.println("######");
+    System.out.println("Aluna: Gabrielle Fernanda Delgado Dias");
+    System.out.println("######");
+    System.out.println("Série:1ano do ensino médio");
+    System.out.println("######");
+    System.out.println("Disciplinas: Lógica Computacional, Análise e Projeto de Sistemas, Ciência da Computação");
+    System.out.println("######");
+    System.out.println("Professores: Lucas Stasiak e Juliano de Morais");
+    System.out.println("######");
+    System.out.println("Eu Gabrielle Dias estou achando o curso em geral muito interessante, estou aprendendo bastante sobre programação, escolhi esse curso pois me interessei por programação e quero me especializar na aréa, essa aréa é uma das melhores atualmente, até porque o mundo está ficando cada vez mais tecnológico. Estou gostando bastante do curso, espero que eu consiga me especializar nesta aréa.");
+    
+  
+  }
+}
